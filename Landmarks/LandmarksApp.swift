ffc12683d7ffa92474dfc4f17bcff89ceae66e6c
Neikkie <neikkie21@outlook.com>
2025-08-25 09:33:40 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shanique Beckford on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
