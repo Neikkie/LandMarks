2609324dab5f0d37156870d7252fb83a9bd75d58
Neikkie <neikkie21@outlook.com>
2025-09-10 22:14:58 -0400

    feat: create a landmark model
    https://developer.apple.com/tutorials/swiftui/building-lists-and-navigation

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,28 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Shanique Beckford on 9/10/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Shanique Beckford on 9/10/25.
+//
+
+import Foundation
